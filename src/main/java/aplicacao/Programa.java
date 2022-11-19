295f31a0b5dcf1c474aad065c260320bc795b394
Vinícius Castelo <vkstello@gmail.com>
2022-11-19 01:20:06 -0300

    insert method with JPA in mySQL Database

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -2,9 +2,36 @@ package aplicacao;
 
 import dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Scanner;
+
 public class Programa {
     public static void main(String[] args) {
-        Pessoa p = new Pessoa(1, "Vinícius");
-        System.out.println(p);
+
+        Scanner sc = new Scanner(System.in);
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        System.out.println();
+        System.out.println("=== CADASTRO DE PESSOAS ===");
+        System.out.println();
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+        System.out.print("E-mail: ");
+        String email = sc.nextLine();
+        Pessoa pessoa = new Pessoa(null, nome, email);
+
+        em.getTransaction().begin();
+        em.persist(pessoa);
+        em.getTransaction().commit();
+
+        System.out.println();
+        System.out.println("Cadastro realizado com sucesso!");
+
+        sc.close();
+
     }
 }
--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -1,15 +1,28 @@
 package dominio;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Pessoa {
+@Entity
+public class Pessoa implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+    private String email;
 
-    public Pessoa(Integer id, String nome) {
+    public Pessoa() {
+    }
+
+    public Pessoa(Integer id, String nome, String email) {
         this.id = id;
         this.nome = nome;
+        this.email = email;
     }
 
     public Integer getId() {
@@ -26,6 +39,14 @@ public class Pessoa {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
@@ -46,6 +67,7 @@ public class Pessoa {
         return "{Pessoa:{" +
                 "id: " + id +
                 ", nome: '" + nome + '\'' +
+                ", email: '" + email + '\'' +
                 '}' + '}';
     }
 
