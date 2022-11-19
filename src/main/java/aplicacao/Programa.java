acb39bbe3b9a7d0848c344da4e456aec462f22b0
Vinícius Castelo <vkstello@gmail.com>
2022-11-19 00:34:47 -0300

    Starting project jpa-maven

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,10 @@
+package aplicacao;
+
+import dominio.Pessoa;
+
+public class Programa {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa(1, "Vinícius");
+        System.out.println(p);
+    }
+}
